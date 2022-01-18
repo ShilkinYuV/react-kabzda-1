8555b0f3e8397c21a229f1d8fd2f982cd5863b6a
ShilkinYuV <51208697+ShilkinYuV@users.noreply.github.com>
2022-01-18 23:41:58 +0300

    tutorial 29

--- src/components/Profile/ProfileInfo/ProfileInfo.js
+++ src/components/Profile/ProfileInfo/ProfileInfo.js
@@ -4,8 +4,7 @@ import s from './ProfileInfo.module.css';
 const ProfileInfo = () => {
     return <div className={s.content}>
         <div>
-            <img
-                src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
+            <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
         </div>
         <div className={s.descriptionBlock}>
             ava + description
