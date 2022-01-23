9c59f753f6c4d6a49b59df4425a3ed43bbc699ae
ShilkinYuV <51208697+ShilkinYuV@users.noreply.github.com>
2022-01-23 17:54:56 +0300

    tutorial 34

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {updateNewPostText} from "./redux/state";
 
 
 const App = (props) => {
@@ -16,7 +17,7 @@ const App = (props) => {
                 <div className='app-wrapper-content'>
                     <Routes>
                         <Route path="/dialogs/*" element={<Dialogs state={props.state.dialogsPage}/>}/>
-                        <Route path="/profile" element={<Profile state={props.state.profilePage}/>}/>
+                        <Route path="/profile" element={<Profile profilePage={props.state.profilePage} addPost={props.addPost} updateNewPostText={updateNewPostText}/>}/>
                     </Routes>
                 </div>
             </div>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,9 +1,12 @@
+import {rerenderEntireTree} from "../render";
+
 let state = {
     profilePage: {
         posts: [
             {id: 1, message: 'Hi, how are you?', likesCount: 12},
             {id: 2, message: 'It\'s my first post', likesCount: 11}
-        ]
+        ],
+        newPostText: ''
     },
     dialogsPage: {
         dialogs: [
@@ -21,7 +24,25 @@ let state = {
             {id: 4, message: 'Yo'},
             {id: 5, message: 'Yo'}
         ]
-    }
+    },
+    sidebar: {}
+}
+
+export let addPost = () => {
+    let newPost = {
+        id: 5,
+        message: state.profilePage.newPostText,
+        likesCount: 0
+    };
+
+    state.profilePage.posts.push(newPost)
+    state.profilePage.newPostText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText
+    rerenderEntireTree(state);
 }
 
 export default state;
